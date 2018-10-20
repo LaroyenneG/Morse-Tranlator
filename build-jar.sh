e432236f5af31b1c58fde055e864d22b208a3b02
laroyenneg <laroyenneg@gmail.com>
2018-10-21 00:56:36 +0200

    jar command build

--- build-jar.sh
+++ build-jar.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 
-javac -classpath . ./src/morse/translator/*.java ./src/*.java
-jar cvmf MANIFEST.MF MorseTranslator.jar *.class ./document/Morse-Tranlator.pdf/ ./src/morse/translator/assets/morse_code.txt
+rm  -r out/
+
+mkdir -p out/production/morse/translator/assets/ 2>/dev/null;
+
+javac -classpath . ./src/morse/translator/*.java ./src/*.java -d out/production/;
+cp ./src/morse/translator/assets/morse_code.txt ./out/production/morse/translator/assets/morse_code.txt;
+
+
+cd ./out/production/;
+jar cvmf ../../MANIFEST.MF MorseTranslator.jar *.class ./morse/translator/*.class  ./morse/translator/assets/morse_code.txt;
+
+exit;
