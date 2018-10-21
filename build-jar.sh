72f1a2923c440fc66c40a73602fe9fd116e5326f
laroyenneg <laroyenneg@gmail.com>
2018-10-21 15:03:24 +0200

    jar command build and document

--- build-jar.sh
+++ build-jar.sh
@@ -3,14 +3,14 @@
 
 rm  -r out/
 
-mkdir -p out/production/morse/translator/assets/ 2>/dev/null;
+mkdir -p out/production/Morse-Translator/morse/translator/assets/ 2>/dev/null;
 
-javac -classpath . ./src/morse/translator/*.java ./src/*.java -d out/production/;
-cp ./src/morse/translator/assets/morse_code.txt ./out/production/morse/translator/assets/morse_code.txt;
-cp ./document/Morse-Tranlator.pdf ./out/production/;
+javac -classpath . ./src/morse/translator/*.java ./src/*.java -d out/production/Morse-Translator/;
+cp ./src/morse/translator/assets/morse_code.txt ./out/production/Morse-Translator/morse/translator/assets/morse_code.txt;
+cp ./document/Morse-Tranlator.pdf ./out/production/Morse-Translator/;
 
 
-cd ./out/production/;
-jar cvmf ../../MANIFEST.MF MorseTranslator.jar *.class ./morse/translator/*.class  ./morse/translator/assets/morse_code.txt ./Morse-Tranlator.pdf ../../src;
+cd ./out/production/Morse-Translator/;
+jar cvmf ../../../MANIFEST.MF MorseTranslator.jar *.class ./morse/translator/*.class  ./morse/translator/assets/morse_code.txt ./Morse-Tranlator.pdf ../../../src;
 
 exit;
