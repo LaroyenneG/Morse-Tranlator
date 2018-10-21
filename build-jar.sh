5d28e45d12dcbd094df8f57cdfdffa2d34cec257
LaroyenneG <laroyenneg@gmail.com>
2018-10-21 18:20:56 +0200

    tex compilation

--- build-jar.sh
+++ build-jar.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 
+
+(cd ./document/; pdflatex Morse-Tranlator.tex);
+
 rm  -r out/
 
 mkdir -p out/production/Morse-Translator/morse/translator/assets/ 2>/dev/null;
 
 javac -classpath . ./src/morse/translator/*.java ./src/*.java -d out/production/Morse-Translator/;
 cp ./src/morse/translator/assets/morse_code.txt ./out/production/Morse-Translator/morse/translator/assets/morse_code.txt;
+
+
 cp ./document/Morse-Tranlator.pdf ./out/production/Morse-Translator/;
 
 
